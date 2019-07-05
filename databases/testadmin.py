6b1fae13f7ee36aebefee838abcc3957a50dbd42
Jake Burton <40278490@live.napier.ac.uk>
2019-07-05 21:10:55 +0100

    05-07-19 Work from Uni towards Honours

--- databases/testadmin.py
+++ databases/testadmin.py
@@ -4,7 +4,6 @@ conn = sqlite3.connect('sensors.db')
 c=conn.cursor()
 c.execute("SELECT id FROM readings ORDER BY id DESC LIMIT 1")
 index = c.fetchall()
-#print(index[0][0]
 i = index[0][0]
 c.execute("SELECT id FROM nodes ORDER BY id DESC LIMIT 1")
 node = c.fetchall()
@@ -14,6 +13,7 @@ for x in range(10):
 	nodeid = random.randint(1,nodecount)
 	temp = random.randint(0,30)
 	hum = random.randint(5,100)
-	c.execute("""INSERT INTO readings VALUES((?),(?),datetime('now'),(?),(?))""",(i,nodeid,temp,hum))
+	c.execute("""INSERT INTO readings VALUES((?),(?),datetime('now'),
+		(?),(?))""",(i,nodeid,temp,hum))
 conn.commit()
 conn.close()
